e5e063a7db2603a0e5f7c4b755d12006b8a634c5
Massick <4ndyraul@gmail.com>
2022-10-09 14:00:17 -0400

    add utils

---
+++ bots/animebot/src/utils/index.ts
@@ -0,0 +1,18 @@
+export const padTo2Digits = (num: number) => {
+    return num.toString().padStart(2, '0')
+}
+
+export const convertMsToTime = (milliseconds: number) => {
+    let seconds = Math.floor(milliseconds / 1000)
+    let minutes = Math.floor(seconds / 60)
+    let hours = Math.floor(minutes / 60)
+    const days = Math.floor(hours / 24)
+
+    seconds = seconds % 60
+    minutes = minutes % 60
+    hours = hours % 24
+
+    return `${days}:${padTo2Digits(hours)}:${padTo2Digits(minutes)}:${padTo2Digits(
+        seconds,
+    )}`
+}
