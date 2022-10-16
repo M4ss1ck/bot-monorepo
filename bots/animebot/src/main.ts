02ed3766fc8a28b40000b4f9e33cc2968412159e
Massick <4ndyraul@gmail.com>
2022-10-16 12:56:34 -0400

    set command list if necessary

--- bots/animebot/src/main.ts
+++ bots/animebot/src/main.ts
@@ -3,6 +3,7 @@ import anime from './middleware/anime.js'
 import commands from './middleware/commands.js'
 // import users from './middleware/createUsers.js'
 import actions from './middleware/actions.js'
+// import inline from './middleware/inline.js'
 
 const bot = new Telegraf(process.env.BOT_TOKEN ?? '')
 
@@ -11,6 +12,39 @@ bot
     .use(anime)
     .use(commands)
     .use(actions)
+// .use(inline)
+
+const commandList = await bot.telegram
+    .getMyCommands()
+    .catch((e) => console.log(e));
+
+if (commandList && !commandList.some((command) => command.command === "myanime")) {
+    bot.telegram.setMyCommands([
+        { command: "myanime", description: "Show your stored anime" },
+        {
+            command: "save",
+            description: "Add new anime to database",
+        },
+        {
+            command: "anime",
+            description: "Search anime in AniList",
+        },
+        {
+            command: "character",
+            description: "Search character in AniList",
+        },
+        {
+            command: "animebd",
+            description: "Search for characters with a birthday today.",
+        },
+        {
+            command: "help",
+            description: "Get help"
+        }
+    ]);
+} else {
+    console.log("No need to update commands");
+}
 
 // Iniciar bot
 bot.launch()
