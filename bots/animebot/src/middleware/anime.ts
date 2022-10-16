da86dd29d02da81248fe286d4da83abd0e822279
Massick <4ndyraul@gmail.com>
2022-10-16 15:22:56 -0400

    fix anime middleware

--- bots/animebot/src/middleware/anime.ts
+++ bots/animebot/src/middleware/anime.ts
@@ -1,4 +1,5 @@
 import { Composer, Markup } from 'telegraf'
+import { logger } from '../logger/index.js'
 
 import type { Anime, AnimeFull, Character } from '../types'
 
@@ -12,26 +13,30 @@ anime.command('anime', async (ctx) => {
     const search = ctx.message.text.replace(/^\/anime((@\w+)?\s+)?/i, '')
     if (search.length > 2) {
         // buscar en AniList
-        const results = await getAnimes(search)
-        const media = results.Page?.media as Anime[]
-        const total = results.Page?.pageInfo?.total as number ?? 1
-        const perPage = results.Page?.pageInfo?.perPage as number ?? 5
-        if (media && media.length > 0) {
-            const buttons = []
-            for (const anime of media)
-                buttons.push([Markup.button.callback(anime.title.romaji ?? 'placeholder text', `getAnime${anime.id}`)])
+        try {
+            const results = await getAnimes(search)
+            const media = results.Page?.media as Anime[]
+            const total = results.Page?.pageInfo?.total as number ?? 1
+            const perPage = results.Page?.pageInfo?.perPage as number ?? 5
+            if (media && media.length > 0) {
+                const buttons = []
+                for (const anime of media)
+                    buttons.push([Markup.button.callback(anime.title.romaji ?? 'placeholder text', `getAnime${anime.id}`)])
 
-            buttons.push([
-                Markup.button.callback('⏭', `AnimPage${2}-${search}`, total / perPage <= 1),
-            ])
+                buttons.push([
+                    Markup.button.callback('⏭', `AnimPage${2}-${search}`, total / perPage <= 1),
+                ])
 
-            const keyboard = Markup.inlineKeyboard(buttons)
-            const text = `Resultados para <b>${search}</b>`
+                const keyboard = Markup.inlineKeyboard(buttons)
+                const text = `Resultados para <b>${search}</b>`
 
-            ctx.replyWithHTML(text, keyboard)
-        }
-        else {
-            ctx.replyWithHTML('No se encontraron resultados o hubo un error')
+                ctx.replyWithHTML(text, keyboard)
+            }
+            else {
+                ctx.replyWithHTML('No se encontraron resultados o hubo un error')
+            }
+        } catch (error) {
+            logger.error(error)
         }
     }
 })
@@ -42,27 +47,31 @@ anime.action(/AnimPage\d+-/i, async (ctx) => {
     const search = ctx.callbackQuery.data?.replace(/AnimPage\d+-/i, '')
     if (search && search.length > 2) {
         // buscar en AniList
-        const results = await getAnimes(search, page)
-        const media = results.Page?.media as Anime[]
-        const total = results.Page?.pageInfo?.total as number ?? 1
-        const perPage = results.Page?.pageInfo?.perPage as number ?? 5
-        if (media && media.length > 0) {
-            const buttons = []
-            for (const anime of media)
-                buttons.push([Markup.button.callback(anime.title.romaji ?? 'placeholder text', `getAnime${anime.id}`)])
+        try {
+            const results = await getAnimes(search, page)
+            const media = results.Page?.media as Anime[]
+            const total = results.Page?.pageInfo?.total as number ?? 1
+            const perPage = results.Page?.pageInfo?.perPage as number ?? 5
+            if (media && media.length > 0) {
+                const buttons = []
+                for (const anime of media)
+                    buttons.push([Markup.button.callback(anime.title.romaji ?? 'placeholder text', `getAnime${anime.id}`)])
 
-            const showPrevBtn = page >= 2
-            const showNextBtn = total / perPage > page
+                const showPrevBtn = page >= 2
+                const showNextBtn = total / perPage > page
 
-            const lastRow = []
-            showPrevBtn && lastRow.push(Markup.button.callback('⏮', `AnimPage${page - 1}-${search}`))
-            showNextBtn && lastRow.push(Markup.button.callback('⏭', `AnimPage${page + 1}-${search}`))
+                const lastRow = []
+                showPrevBtn && lastRow.push(Markup.button.callback('⏮', `AnimPage${page - 1}-${search}`))
+                showNextBtn && lastRow.push(Markup.button.callback('⏭', `AnimPage${page + 1}-${search}`))
 
-            buttons.push(lastRow)
+                buttons.push(lastRow)
 
-            ctx.editMessageReplyMarkup({
-                inline_keyboard: buttons,
-            })
+                ctx.editMessageReplyMarkup({
+                    inline_keyboard: buttons,
+                })
+            }
+        } catch (error) {
+            logger.error(error)
         }
     }
 })
@@ -71,64 +80,78 @@ anime.action(/getAnime/, async (ctx) => {
     const animeId = parseInt(ctx.callbackQuery.data?.replace('getAnime', '') ?? '')
     if (!isNaN(animeId)) {
         // buscar en AniList
-        const results = await getAnime(animeId)
-        const media = results.Media as AnimeFull
-        if (media) {
-            const caption = `<b>${media.title.romaji ?? 'Title'}</b> (${media.id})
+        try {
+            const results = await getAnime(animeId)
+            const media = results.Media as AnimeFull
+            if (media) {
+                const caption = `<b>${media.title.romaji ?? 'Title'}</b> (${media.id})
       Year: ${media.seasonYear ?? 'n/a'}  Episodes: ${media.episodes ?? 'n/a'}
       
       <i>${media.description.replace(/<br>/g, '') ?? 'description n/a'}`
 
-            const cover = media.coverImage.large
+                const cover = media.coverImage.large
 
-            ctx.replyWithPhoto(cover, {
-                parse_mode: 'HTML',
-                caption: `${caption.slice(0, 1020)}</i>`,
-            })
-        }
-        else {
-            ctx.replyWithHTML('No se encontraron resultados o hubo un error')
+                !ctx.callbackQuery.inline_message_id
+                    ? ctx.replyWithPhoto(cover, {
+                        parse_mode: 'HTML',
+                        caption: `${caption.slice(0, 1020)}</i>`,
+                    })
+                    : ctx.editMessageText(`${caption.slice(0, 4090)}</i>`, { parse_mode: "HTML" })
+            }
+            else {
+                ctx.replyWithHTML('No se encontraron resultados o hubo un error')
+            }
+        } catch (error) {
+            logger.error(error)
         }
     }
 })
 
 anime.command('animebd', async (ctx) => {
-    const results = await getIsBirthdayCharacters()
-    const characters = results.Page?.characters as Character[]
-
-    if (characters && characters.length > 0) {
-        const buttons = []
-        for (const char of characters)
-            buttons.push([Markup.button.callback(char.name.full ?? 'error con el nombre', `getCharacter${char.id}`)])
-
-        const keyboard = Markup.inlineKeyboard(buttons)
-        const text = 'Personajes que celebran su cumpleaños hoy\n'
-
-        ctx.replyWithHTML(text, keyboard)
-    }
-})
-
-anime.command('character', async (ctx) => {
-    const search = ctx.message.text.replace(/^\/character((@\w+)?\s+)?/i, '')
-    if (search.length > 2) {
-        const results = await getCharacters(search)
+    try {
+        const results = await getIsBirthdayCharacters()
         const characters = results.Page?.characters as Character[]
-        const total = results.Page?.pageInfo?.total as number ?? 1
-        const perPage = results.Page?.pageInfo?.perPage as number ?? 5
 
         if (characters && characters.length > 0) {
             const buttons = []
             for (const char of characters)
                 buttons.push([Markup.button.callback(char.name.full ?? 'error con el nombre', `getCharacter${char.id}`)])
 
-            buttons.push([
-                Markup.button.callback('⏭', `CharPage${2}-${search}`, total / perPage <= 1),
-            ])
-
             const keyboard = Markup.inlineKeyboard(buttons)
-            const text = `Resultados para <i>${search}</i>`
+            const text = 'Personajes que celebran su cumpleaños hoy\n'
 
             ctx.replyWithHTML(text, keyboard)
+        }
+    } catch (error) {
+        logger.error(error)
+    }
+})
+
+anime.command('character', async (ctx) => {
+    const search = ctx.message.text.replace(/^\/character((@\w+)?\s+)?/i, '')
+    if (search.length > 2) {
+        try {
+            const results = await getCharacters(search)
+            const characters = results.Page?.characters as Character[]
+            const total = results.Page?.pageInfo?.total as number ?? 1
+            const perPage = results.Page?.pageInfo?.perPage as number ?? 5
+
+            if (characters && characters.length > 0) {
+                const buttons = []
+                for (const char of characters)
+                    buttons.push([Markup.button.callback(char.name.full ?? 'error con el nombre', `getCharacter${char.id}`)])
+
+                buttons.push([
+                    Markup.button.callback('⏭', `CharPage${2}-${search}`, total / perPage <= 1),
+                ])
+
+                const keyboard = Markup.inlineKeyboard(buttons)
+                const text = `Resultados para <i>${search}</i>`
+
+                ctx.replyWithHTML(text, keyboard)
+            }
+        } catch (error) {
+            logger.error(error)
         }
     }
 })
@@ -139,28 +162,32 @@ anime.action(/CharPage\d+-/i, async (ctx) => {
     console.log('editando el mensaje', page)
     const search = ctx.callbackQuery.data?.replace(/CharPage\d+-/i, '')
     if (search && search.length > 2) {
-        const results = await getCharacters(search, page)
-        const characters = results.Page?.characters as Character[]
-        const total = results.Page?.pageInfo?.total as number ?? 1
-        const perPage = results.Page?.pageInfo?.perPage as number ?? 5
+        try {
+            const results = await getCharacters(search, page)
+            const characters = results.Page?.characters as Character[]
+            const total = results.Page?.pageInfo?.total as number ?? 1
+            const perPage = results.Page?.pageInfo?.perPage as number ?? 5
 
-        if (characters && characters.length > 0) {
-            const buttons = []
-            for (const char of characters)
-                buttons.push([Markup.button.callback(char.name.full ?? 'error con el nombre', `getCharacter${char.id}`)])
+            if (characters && characters.length > 0) {
+                const buttons = []
+                for (const char of characters)
+                    buttons.push([Markup.button.callback(char.name.full ?? 'error con el nombre', `getCharacter${char.id}`)])
 
-            const showPrevBtn = page >= 2
-            const showNextBtn = total / perPage > page
+                const showPrevBtn = page >= 2
+                const showNextBtn = total / perPage > page
 
-            const lastRow = []
-            showPrevBtn && lastRow.push(Markup.button.callback('⏮', `CharPage${page - 1}-${search}`))
-            showNextBtn && lastRow.push(Markup.button.callback('⏭', `CharPage${page + 1}-${search}`))
+                const lastRow = []
+                showPrevBtn && lastRow.push(Markup.button.callback('⏮', `CharPage${page - 1}-${search}`))
+                showNextBtn && lastRow.push(Markup.button.callback('⏭', `CharPage${page + 1}-${search}`))
 
-            buttons.push(lastRow)
+                buttons.push(lastRow)
 
-            ctx.editMessageReplyMarkup({
-                inline_keyboard: buttons,
-            })
+                ctx.editMessageReplyMarkup({
+                    inline_keyboard: buttons,
+                })
+            }
+        } catch (error) {
+            logger.error(error)
         }
     }
 })
@@ -169,23 +196,29 @@ anime.action(/getCharacter/, async (ctx) => {
     const characterId = parseInt(ctx.callbackQuery.data?.replace('getCharacter', '') ?? '')
     if (!isNaN(characterId)) {
         // buscar en AniList
-        const results = await getCharacter(characterId)
-        const character = results.Character as Character
-        if (character) {
-            const caption = `<a href="${character.siteUrl}">${character.name.full ?? 'Nombre'}</a> (${character.id})
+        try {
+            const results = await getCharacter(characterId)
+            const character = results.Character as Character
+            if (character) {
+                const caption = `<a href="${character.siteUrl}">${character.name.full ?? 'Nombre'}</a> (${character.id})
       Age: ${character.age ?? 'n/a'}  Gender: ${character.gender ?? 'n/a'}
       
       <i>${character.description.replace(/<br>/g, '') ?? 'description n/a'}`
 
-            const cover = character.image.large
+                const cover = character.image.large
 
-            ctx.replyWithPhoto(cover, {
-                parse_mode: 'HTML',
-                caption: `${caption.slice(0, 1020)}</i>`,
-            })
-        }
-        else {
-            ctx.replyWithHTML('No se encontraron resultados o hubo un error')
+                !ctx.callbackQuery.inline_message_id
+                    ? ctx.replyWithPhoto(cover, {
+                        parse_mode: 'HTML',
+                        caption: `${caption.slice(0, 1020)}</i>`,
+                    })
+                    : ctx.editMessageText(`${caption.slice(0, 4090)}</i>`, { parse_mode: "HTML" })
+            }
+            else {
+                ctx.replyWithHTML('No se encontraron resultados o hubo un error')
+            }
+        } catch (error) {
+            logger.error(error)
         }
     }
 })
