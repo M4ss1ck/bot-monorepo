781d939fdbc9cdc0180184fa9c3ad26d1f55a2b8
Massick <4ndyraul@gmail.com>
2022-11-06 00:54:11 -0400

    store jobs in db

--- bots/animebot/src/scheduler/index.ts
+++ bots/animebot/src/scheduler/index.ts
@@ -1,23 +1,34 @@
 import schedule from 'node-schedule'
-// import { prisma } from "../db/prisma.js"
+import { prisma } from "../db/prisma.js"
 import { logger } from '../logger/index.js'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime.js'
 
 dayjs.extend(relativeTime)
 
-export const scheduled = (id: string, cronExpression: string | number | Date, func: () => void) => {
+export const scheduled = async (id: string, cronExpression: string | number | Date, func: () => void, text?: string) => {
     logger.info('adding new scheduled task\n', cronExpression)
-    let text: string
+    let jobText: string
     const job = getScheduled(id)
     if (job) {
-        text = 'Job is already scheduled'
+        jobText = 'Job is already scheduled'
     } else {
         const newjob = schedule.scheduleJob(id, cronExpression, func)
         newjob && logger.success(`Job ${id} will run ${dayjs(newjob.nextInvocation()).fromNow()}`)
-        text = newjob ? `Job ${id} will run ${dayjs(newjob.nextInvocation()).fromNow()}` : 'Job failed for unknown reasons. Developer bad'
+        jobText = newjob ? `Job ${id} will run ${dayjs(newjob.nextInvocation()).fromNow()}` : 'Job failed for unknown reasons. Developer bad'
+
+        logger.info('Check if date is at least a day in a future or a cron expression to store job in db\n', dayjs(cronExpression).isAfter(dayjs().add(1, 'd')))
+        if (newjob && (typeof cronExpression === 'string' || dayjs(cronExpression).isAfter(dayjs().add(1, 'd')))) {
+            await prisma.job.create({
+                data: {
+                    id: id,
+                    date: String(cronExpression),
+                    text: text ?? ''
+                }
+            })
+        }
     }
-    return text
+    return jobText
 }
 
 export const getScheduled = (id: string) => {
