09e80349783ae38d4a11fa95a2f9af1654457f17
Massick <4ndyraul@gmail.com>
2022-11-21 13:19:12 -0500

    fix userId and add repeat button

--- bots/animebot/src/utils/index.ts
+++ bots/animebot/src/utils/index.ts
@@ -1,7 +1,9 @@
 import { prisma } from "../db/prisma.js"
 import { scheduled } from "../scheduler/index.js"
 import type { Telegraf } from "telegraf"
+import { Markup } from "telegraf"
 import { logger } from "../logger/index.js"
+import dayjs from 'dayjs'
 
 export const padTo2Digits = (num: number) => {
     return num.toString().padStart(2, '0')
@@ -38,10 +40,23 @@ export const convertMsToRelativeTime = (milliseconds: number) => {
 export const runScheduled = async (bot: Telegraf) => {
     const jobs = await prisma.job.findMany()
     for (const job of jobs) {
-        const [, userId] = job.id.split(':')
-        const jobText = await scheduled(job.id, /^\d+$/.test(job.date) ? Number(job.date) : job.date, () => {
-            bot.telegram.sendMessage(userId, job.text)
-        }, job.text)
-        logger.success(jobText, '\n', job.text)
+        const userId = job.id.split(':').pop()
+        if (userId) {
+            const buttons = []
+            // check if job id starts with anime id
+            if (/^\d+$/g.test(job.id.split(':')[0])) {
+                const animeId = job.id.split(':')[0]
+                const date = job.id.split(':')[1]
+                buttons.push(Markup.button.callback('Repeat next week', `a_scheduler:${animeId}:${dayjs(Number(date)).add(7, 'days').valueOf()}:${userId}`))
+            } else {
+                const date = job.id.split(':')[1]
+                buttons.push(Markup.button.callback('Is date correct?', `check_date:${date}`))
+            }
+            const keyboard = Markup.inlineKeyboard(buttons)
+            const jobText = await scheduled(job.id, /^\d+$/.test(job.date) ? Number(job.date) : job.date, () => {
+                bot.telegram.sendMessage(userId, job.text, keyboard)
+            }, job.text)
+            logger.success(jobText, '\n', job.text)
+        }
     }
 }
